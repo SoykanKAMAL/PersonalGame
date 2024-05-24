b5f299c0b399fbaa98e092c5633e82e2b4e02957
Soykan KAMAL <58806238+SoykanKAMAL@users.noreply.github.com>
2024-05-24 14:22:14 +0300

    Score and Ui managers implemented
    Core functionality for ui and score manager are finished

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int matchScoreReward = 100;
+    private int _score = 0;
+    private int _combo = 1;
+
+    public static Action<int> onScoreUpdated;
+    public static Action<int> onComboUpdated;
+
+    private void OnEnable()
+    {
+        GameManager.onCardsMatched += IncreaseScore;
+        GameManager.onCardsMatched += IncreaseCombo;
+        GameManager.onCardsMismatched += ResetCombo;
+    }
+
+    private void IncreaseScore()
+    {
+        _score += matchScoreReward * _combo;
+        onScoreUpdated?.Invoke(_score);
+    }
+
+    private void ResetScore()
+    {
+        _score = 0;
+        onScoreUpdated?.Invoke(_score);
+    }
+
+    private void IncreaseCombo()
+    {
+        _combo++;
+        onComboUpdated?.Invoke(_combo);
+    }
+
+    private void ResetCombo()
+    {
+        _combo = 1;
+        onComboUpdated?.Invoke(_combo);
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI comboText;
+
+    private void OnEnable()
+    {
+        ScoreManager.onScoreUpdated += UpdateScore;
+        ScoreManager.onComboUpdated += UpdateCombo;
+
+        UpdateScore(0);
+        UpdateCombo(1);
+    }
+
+    private void UpdateScore(int score)
+    {
+        scoreText.text = $"Score: {score}";
+    }
+
+    private void UpdateCombo(int combo)
+    {
+        if(combo > 1)
+        {
+            comboText.text = $"Combo: {combo}x";
+        }
+        else
+        {
+            comboText.text = string.Empty;
+        }
+    }
+}
